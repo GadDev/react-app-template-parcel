84547745a3d0bee6cbadbc8eac75b62ea23efe88
algadaix <alexandre@qured.com>
2021-02-16 16:51:15 +0000

    push template parcel react

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useState } from 'react';
+
+import '../style.css';
+const App = () => {
+	return <div className='App'>React app</div>;
+};
+
+export default App;
